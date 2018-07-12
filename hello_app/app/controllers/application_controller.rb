bdaf770d57437f8f3cacac7ab6c0af6950136fbe
yumechi <yumechi0525@gmail.com>
2018-07-13 01:18:01 +0900

    hello, world close #3

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def hello
+        render html: "やあ、待ったかい？"
+    end
+
+    def goodbye
+        render html: "Good night, 今日はもう眠ろう"
+    end
 end
