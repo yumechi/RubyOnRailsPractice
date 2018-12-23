05d5107d795f708a32313a4d8b6032ab5fb1a509
yumechi <yumechi0525@gmail.com>
2018-12-24 01:51:48 +0900

    force push option

--- build_cmd.rb
+++ build_cmd.rb
@@ -3,23 +3,25 @@ require 'json'
 
 option = {}
 OptionParser.new do |opt|
-  opt.on('-d dirname', 'input deploy target dirname') {|v| option[:f] = v}
+  opt.on('-d dirname', 'input deploy target dirname') {|v| option[:d] = v}
+  opt.on('-f', 'force deplhy target dirname') {|v| option[:f] = v}
 
   opt.parse!(ARGV)
 end
 
-if option[:f]
-  dir_name = option[:f]
+if option[:d]
+  dir_name = option[:d]
+  force_flag = option[:f]
   map_data = Hash.new
   File.open('map_data.json') do |j|
     map_data = JSON.load(j)
   end
   site_name = map_data[dir_name]
-  if site_name
+  if force_flag or site_name
     puts "try build #{dir_name}"
     result = `git subtree push --prefix #{dir_name}/ heroku master`
     if $? == 0
-      puts "show page: https://#{site_name}.herokuapp.com/"
+      puts site_name ? "show page: https://#{site_name}.herokuapp.com/" : "build success"
     else
       puts "Error, #{$?}"
     end
