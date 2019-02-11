5dbd8478fd3573a4943bf216d1e07aa5106cf1b3
yumechi <yumechi0525@gmail.com>
2019-02-11 17:14:08 +0900

    update build_cmd

--- build_cmd.rb
+++ build_cmd.rb
@@ -43,10 +43,13 @@ if option[:d]
   site_name = map_data[dir_name]
   if force_flag or site_name
     exec_command("heroku maintenance:on", "maintenance on") if not maint_flag
-    r = exec_command("git push heroku \`git subtree split --prefix #{dir_name} master\`:master --force",
-                     "try build #{dir_name}",
-                     site_name ? "show page: https://#{site_name}.herokuapp.com/" : "build success")
+    exec_command("git push heroku \`git subtree split --prefix #{dir_name} master\`:master --force",
+                  "try build #{dir_name}",
+                  site_name ? "show page: https://#{site_name}.herokuapp.com/" : "build success")
+    exec_command("heroku pg:reset DATABASE", "reset database") if not migrate_flag
     exec_command("heroku run rails db:migrate", "migrate!") if not migrate_flag
+    exec_command("heroku run rails db:seed", "init data") if not migrate_flag
+    exec_command("heroku restart", "heroku restart") if not migrate_flag
     exec_command("heroku maintenance:off", "maintenance off") if not maint_flag
   else
     puts "Error: not found #{dir_name}"
